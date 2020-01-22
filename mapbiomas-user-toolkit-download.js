5870901d6f7c28e7a15c0f21d30f875bc18478a7
joaovsiqueira1 <joaovsiqueira1@gmail.com>
2020-01-22 21:22:23 +0000

    Updated mapbiomas-user-toolkit-download.js (added 3 lines, deleted 2 lines).

--- mapbiomas-user-toolkit-download.js
+++ mapbiomas-user-toolkit-download.js
@@ -30,7 +30,7 @@ var App = {
         logo: logos.mapbiomas,
         assets: {
             integration: 'projects/mapbiomas-workspace/public/collection4/mapbiomas_collection40_integration_v1',
-            transitions: 'projects/mapbiomas-workspace/public/collection4/mapbiomas_collection40_transitions_v2',
+            transitions: 'projects/mapbiomas-workspace/public/collection4/mapbiomas_collection40_transitions_v3',
             vectors: [
                 'projects/mapbiomas-workspace/AUXILIAR/areas-protegidas',
                 'projects/mapbiomas-workspace/AUXILIAR/municipios-2016',
@@ -97,7 +97,8 @@ var App = {
                 "2000_2005", "2005_2010", "2010_2015", "2015_2018",
                 "1990_2000", "2000_2010", "2010_2018", "1985_2018",
                 "2008_2017", "2012_2018", "1994_2002", "2002_2010",
-                "2010_2016", "2008_2018", "1986_2015", "2001_2016"
+                "2010_2016", "2008_2018", "1986_2015", "2001_2016",
+                "1996_2015"
             ]
         },
         bandsNames: {
