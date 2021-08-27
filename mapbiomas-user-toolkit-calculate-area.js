01a106e8aec2e793ec7933a67b7d97300f22cf47
joaovsiqueira1 <joaovsiqueira1@gmail.com>
2021-08-27 21:53:18 +0000

    Updated mapbiomas-user-toolkit-calculate-area.js (added 2 lines, deleted 2 lines).

--- mapbiomas-user-toolkit-calculate-area.js
+++ mapbiomas-user-toolkit-calculate-area.js
@@ -8,7 +8,7 @@
  */
 
 // Asset mapbiomas
-var asset = "projects/mapbiomas-workspace/public/collection5/mapbiomas_collection50_integration_v1";
+var asset = "projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_integration_v1";
 
 // Asset of regions for which you want to calculate statistics
 var assetTerritories = "projects/mapbiomas-workspace/AUXILIAR/biomas-estados-2016-raster";
@@ -22,7 +22,7 @@ var years = [
     '1993', '1994', '1995', '1996', '1997', '1998', '1999', '2000',
     '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
     '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
-    '2017', '2018', '2019'
+    '2017', '2018', '2019', '2020'
 ];
 
 // Define a Google Drive output folder 
