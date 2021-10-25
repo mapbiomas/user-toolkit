a674e8d3dfb0d4d913f3824fb8a9df6ae9bc2b0f
joaovsiqueira1 <joaovsiqueira1@gmail.com>
2021-10-25 18:29:47 +0000

    Updated mapbiomas-user-toolkit-pasture.js (added 124 lines, deleted 94 lines).

--- mapbiomas-user-toolkit-pasture.js
+++ mapbiomas-user-toolkit-pasture.js
@@ -11,6 +11,7 @@
  *
  * @version
  *    1.0.0 - First release
+ *    1.1.0 - Pasture Quality - Collection 6.0
  * 
  * @see
  *      Get the MapBiomas exported data in your "Google Drive/MAPBIOMAS-EXPORT" folder
@@ -105,7 +106,7 @@ var App = {
 
     options: {
 
-        version: '1.0.0',
+        version: '1.1.0',
 
         logo: logos.mapbiomas,
 
@@ -142,38 +143,82 @@ var App = {
 
         tables: {
             'mapbiomas-brazil': [
-                'projects/mapbiomas-workspace/AUXILIAR/areas-protegidas',
-                'projects/mapbiomas-workspace/AUXILIAR/biomas-2019',
-                'projects/mapbiomas-workspace/AUXILIAR/bacias-nivel-1',
-                'projects/mapbiomas-workspace/AUXILIAR/bacias-nivel-2',
-                'projects/mapbiomas-workspace/AUXILIAR/estados-2017',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-AC',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-AL',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-AM',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-AP',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-BA',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-CE',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-DF',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-ES',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-GO',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-MA',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-MG',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-MS',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-MT',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-PA',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-PB',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-PE',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-PI',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-PR',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-RJ',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-RN',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-RO',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-RR',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-RS',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-SC',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-SE',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-SP',
-                'projects/mapbiomas-workspace/AUXILIAR/MUNICIPIOS/municipios-TO',
+                {
+                    'label': 'atlantic_forest_law',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/atlantic_forest_law',
+                },
+                {
+                    'label': 'biome',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/biome',
+                },
+                {
+                    'label': 'biosphere_reserve',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/biosphere_reserve',
+                },
+                {
+                    'label': 'city',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/city',
+                },
+                {
+                    'label': 'country',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/country',
+                },
+                {
+                    'label': 'federal_conservation_units_integral_protection',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/federal_conservation_units_integral_protection',
+                },
+                {
+                    'label': 'federal_conservation_units_sustainable_use',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/federal_conservation_units_sustainable_use',
+                },
+                {
+                    'label': 'indigenous_land',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/indigenous_land',
+                },
+                {
+                    'label': 'legal_amazon',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/legal_amazon',
+                },
+                {
+                    'label': 'level_1_drainage_basin',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/level_1_drainage_basin',
+                },
+                {
+                    'label': 'level_1_drainage_basin_pnrh',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/level_1_drainage_basin_pnrh',
+                },
+                {
+                    'label': 'level_2_drainage_basin',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/level_2_drainage_basin',
+                },
+                {
+                    'label': 'level_2_drainage_basin_pnrh',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/level_2_drainage_basin_pnrh',
+                },
+                {
+                    'label': 'quilombo',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/quilombo',
+                },
+                {
+                    'label': 'semiarid',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/semiarid',
+                },
+                {
+                    'label': 'settlement',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/settlement',
+                },
+                {
+                    'label': 'state',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/state',
+                },
+                {
+                    'label': 'state_conservation_units_integral_protection',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/state_conservation_units_integral_protection',
+                },
+                {
+                    'label': 'state_conservation_units_sustainable_use',
+                    'value': 'projects/mapbiomas-workspace/AUXILIAR/ESTATISTICAS/COLECAO6/state_conservation_units_sustainable_use',
+                },
             ],
             // 'mapbiomas-amazon': [
             //     'projects/mapbiomas-raisg/DATOS_AUXILIARES/VECTORES/limite-raisg-2',
@@ -200,15 +245,43 @@ var App = {
 
         collections: {
             'mapbiomas-brazil': {
-                'collection-5.0': {
-                    'assets': { //TODO: Inserir os assets públicos
-                        'pasture_quality': 'projects/mapbiomas-workspace/public/collection5/mapbiomas_collection50_pasture_quality_v1',
+                // 'collection-5.0': {
+                //     'assets': {
+                //         'pasture_quality': 'projects/mapbiomas-workspace/public/collection5/mapbiomas_collection50_pasture_quality_v1',
+                //         // 'pasture': '',
+                //     },
+
+                //     'periods': {
+                //         'pasture_quality': [
+                //             '2010', '2018',
+                //         ],
+                //         'pasture': [
+                //             '1988', '1989', '1990', '1991',
+                //             '1992', '1993', '1994', '1995',
+                //             '1996', '1997', '1998', '1999',
+                //             '2000', '2001', '2002', '2003',
+                //             '2004', '2005', '2006', '2007',
+                //             '2008', '2009', '2010', '2011',
+                //             '2012', '2013', '2014', '2015',
+                //             '2016', '2017',
+                //         ],
+
+                //     },
+                // },
+                'collection-6.0': {
+                    'assets': {
+                        'pasture_quality': 'projects/mapbiomas-workspace/public/collection6/mapbiomas_collection60_pasture_quality_v1',
                         // 'pasture': '',
                     },
 
                     'periods': {
                         'pasture_quality': [
-                            '2010', '2018',
+                            '2000', '2001', '2002', '2003',
+                            '2004', '2005', '2006', '2007',
+                            '2008', '2009', '2010', '2011',
+                            '2012', '2013', '2014', '2015',
+                            '2016', '2017', '2018', '2019',
+                            '2020'
                         ],
                         'pasture': [
                             '1988', '1989', '1990', '1991',
@@ -267,7 +340,7 @@ var App = {
         ranges: {
             'pasture_quality': {
                 'min': 1,
-                'max': 4
+                'max': 3
             },
             'pasture': {
                 'min': 0,
@@ -276,12 +349,11 @@ var App = {
 
         },
 
-        palette: { //TODO: Criar paleta de cores para visualização
+        palette: {
             'pasture_quality': [
-                '#c1786c', // [1] Strong
-                '#ffa16b', // [2] Medium
-                '#fffd85', // [3] Low
-                '#2874d2', // [4] Absent
+                '#A61C00', // 1. Severe degradation
+                '#FDAE61', // 2. Moderate degradation
+                '#2D7BB6', // 3. No degraded
             ],
 
             'pasture': [
@@ -302,61 +374,20 @@ var App = {
         className: {
 
             'pasture_quality': {
-                1: 'Strong',
-                2: 'Medium',
-                3: 'Low',
-                4: 'Absent',
+                1: 'Severe degradation',
+                2: 'Moderate degradation',
+                3: 'No degraded',
             },
-            'classification': {
-                1: "Forest",
-                2: "Natural Forest",
-                3: "Forest Formation",
-                4: "Savanna Formation",
-                5: "Magrove",
-                9: "Forest Plantation",
-                10: "Non Forest Natural Formation",
-                11: "Wetland",
-                12: "Grassland",
-                32: "Salt flat",
-                29: "Rocky outcrop",
-                13: "Other Non Forest Natural Formation",
-                14: "Farming",
-                15: "Pasture",
-                18: "Agriculture",
-                19: "Temporary Crops",
-                39: "Soy Beans",
-                20: "Sugar Cane",
-                40: "Rice",
-                41: "Mosaic of Crops",
-                42: "Coffe",
-                43: "Citrus",
-                44: "Cashew",
-                45: "Other",
-                36: "Perennial Crops",
-                21: "Mosaic of Agriculture and Pasture",
-                22: "Non vegetated area",
-                24: "Urban Infrastructure",
-                30: "Mining",
-                23: "Beach and Dune",
-                25: "Other Non Vegetated Area",
-                26: "Water",
-                33: "River, Lake and Ocean",
-                37: "Artificial Water Body",
-                38: "Water Reservoirs",
-                31: "Aquaculture",
-                27: "Non Observed",
-                0: "Non Observed",
-            }
+
         },
 
         legend: {
             params: {
                 "title": 'Legend',
                 "layers": [
-                    ["#c1786c", 1, 'Strong',],
-                    ["#ffa16b", 2, 'Medium',],
-                    ["#fffd85", 3, 'Low',],
-                    ["#2874d2", 4, 'Absent',],
+                    ['#A61C00', 1, 'Severe degradation',],
+                    ['#FDAE61', 2, 'Moderate degradation',],
+                    ['#2D7BB6', 3, 'No degraded',],
                 ],
                 "style": {
                     "backgroundColor": "#ffffff",
@@ -386,7 +417,6 @@ var App = {
         var imageLayer = ui.Map.Layer({
             'eeObject': App.options.data.pasture_quality,
             'visParams': {
-                // 'bands': ['b1'], // TODO: ajustar o nome das bandas
                 'bands': ['pasture_quality_' + year],
                 'palette': App.options.palette.pasture_quality,
                 'min': App.options.ranges.pasture_quality.min,
