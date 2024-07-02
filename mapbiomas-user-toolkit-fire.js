f8f4c4f3fe8ef8b988e4e3db1a47f051ec3d4b9d
wallace.silva <wallace.silva@ipam.org.br>
2024-07-02 03:50:38 +0000

    Updated mapbiomas-user-toolkit-fire.js (added 1 line, deleted 1 line).

--- mapbiomas-user-toolkit-fire.js
+++ mapbiomas-user-toolkit-fire.js
@@ -29,7 +29,7 @@
  *          - Redesenhando padrão do nome no export && atualização da função formatName, com replaces mais agressivos
  *          - Alterando o plot da feature para uma unica linha
  *          - Alterando posição da escolha da collection e adaptando o codigo a isso
- *          - Removendo labels de loading
+ *          - Alterando o Map.clear() Map.layers().reset([]) e Removendo labels de loading
  *
  * 
  * @see
